f23c891a61e85121b6324de0d2fbbc2c4bbc0d1e
Roshaan Asif <roshaanasif57@gmail.com>
2026-01-01 12:55:28 +0500

    hackathon

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+  tailwind.config = {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Helvetica', 'sans-serif'],
+            ui: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif']
+          }
+        }
+      }
+    }
